2c7de58ee40a5b2ac787cb6a6505c37af513c2fb
EliasDjemaa <92042793+EliasDjemaa@users.noreply.github.com>
2022-04-07 22:18:06 +0100

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Head from 'next/head'
+import {Categories, PostCard, PostWidgets} from '../components';
+import {getPosts} from '../services'
+import Navbar from '../components/navbar/Navbar'
+
+
+
+
+export default function Home({ posts }:{posts:any}) {
+  return (
+    <div className="container mx-auto px-10 mb-">
+      <Head>
+        <title>Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        <div className='lg:col-span-8 col-span-1'>
+          {posts.map((post: { node: any; title: React.Key | null | undefined; }) => <PostCard post={post.node} key={post.title}/>)}
+          
+        </div>
+        <div className='lg:col-span-4 col-span-1 '>
+          <div className='lg:sticky relative top-8'>
+            <PostWidgets categories={undefined} slug={undefined} />
+            <Categories />
+          </div>
+
+      </div>
+      </div>
+
+      
+ 
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
+}
+
